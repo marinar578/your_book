4ba006dd61e9a041bac4d3ef5c8b8c09aff01a6a
Marina Rozova <marinar578@gmail.com>
2016-03-02 18:26:01 -0500

    add nyc maps functionality

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -29,84 +29,94 @@ console.log("loaded");
  
     };
 
+    $('.button').click(function(){
+        var itemId = $(this).attr('id');
+        console.log('clicked');
 
-        $('.button').click(function(){
-            var itemId = $(this).attr('id');
-            console.log('clicked');
+        $.ajax({
+            type: "GET",
+            url: "https://www.googleapis.com/books/v1/volumes?q="+spliceLit(timePeriods[itemId]),
+            success: function(data){
+                if(data){
+                    for(i=0; i < data.items.length; i++){
+                        dataArray[itemId].push(data.items[i]);
+                    };// for loop
+                }// if
+            }// success
+        })// ajax call
 
-            $.ajax({
-                type: "GET",
-                url: "https://www.googleapis.com/books/v1/volumes?q="+spliceLit(timePeriods[itemId]),
-                success: function(data){
-                    if(data){
-                        for(i=0; i < data.items.length; i++){
-                            dataArray[itemId].push(data.items[i]);
-                        };// for loop
-                    }// if
-                }// success
-            })// ajax call
+        var dataObj = spliceLit(dataArray[itemId]);
 
-            var dataObj = spliceLit(dataArray[itemId]);
-
-            var image = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/No_shield_available.svg/200px-No_shield_available.svg.png";
-            var authors = "unknown"
-            var title = dataObj[0].volumeInfo.title
-            var link = dataObj[0].volumeInfo.previewLink;
-            
-            if(dataObj[0].volumeInfo.imageLinks){
-                image = dataObj[0].volumeInfo.imageLinks.thumbnail;
-            };
-            if(dataObj[0].volumeInfo.authors){
-                var authors = dataObj[0].volumeInfo.authors.join(', ');
-            };
-            
-
-            $('.book-container').prepend("<div class='bookcard mdl-grid'>");
-            $('.book-container').children().first().append('<div  class="img mdl-cell mdl-cell--4-col"></div>');
-            $('.bookcard div:eq(0)').append('<img src='+image+'>');
-            $('.book-container').children().first().append('<div  class="book-info mdl-cell mdl-cell--6-col"></div>');
-            $('.bookcard div:eq(1)').append('<p id="title"><strong>Title:</strong> '+ title +'</p>');
-            $('.bookcard div:eq(1)').children().first().append('<p id="authors"><strong>Authors:</strong> '+authors+'</p>');
-            $('.bookcard div:eq(1)').children().first().append('<p><a href="'+link+'" target="_blank">Book Preview</a></p>');
-        })
-
-
-    // $('#old').click(function(){
-    //     console.log("clicked");
-
-    //     $.ajax({
-    //         type: "GET",
-    //         url: "https://www.googleapis.com/books/v1/volumes?q="+spliceLit(timePeriods.oldEnglish),
-    //         success: function(data){
-    //             if(data){
-    //                 for(i=0; i < data.items.length; i++){
-    //                     dataArray.oldEnglish.push(data.items[i]);
-    //                 };// for loop
-    //             }// if
-    //         }// success
-    //     })// ajax call
-
-    //     var dataObj = spliceLit(dataArray.oldEnglish);
-
-    //     var image = "https://i.ytimg.com/vi/rg-3a6Hy-yc/maxresdefault.jpg";
-    //     var authors = "unknown"
-    //     var title = dataObj[0].volumeInfo.title
-    //     var link = dataObj[0].volumeInfo.previewLink;
+        var image = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/No_shield_available.svg/200px-No_shield_available.svg.png";
+        var authors = "unknown"
+        var title = dataObj[0].volumeInfo.title
+        var link = dataObj[0].volumeInfo.previewLink;
         
-    //     if(dataObj[0].volumeInfo.imageLinks){
-    //         image = dataObj[0].volumeInfo.imageLinks.thumbnail;
-    //     };
-    //     if(dataObj[0].volumeInfo.authors){
-    //         var authors = dataObj[0].volumeInfo.authors.join(', ');
-    //     };
+        if(dataObj[0].volumeInfo.imageLinks){
+            image = dataObj[0].volumeInfo.imageLinks.thumbnail;
+        };
+        if(dataObj[0].volumeInfo.authors){
+            var authors = dataObj[0].volumeInfo.authors.join(', ');
+        };
         
 
-    //     $('.twelve').prepend("<div class='bookcard'>");
-    //     $('.twelve').children().first().append('<img src='+image+'>');
-    //     $('.twelve').children().first().append('<p id="title">'+ title +'</p>');
-    //     $('.twelve').children().first().append('<p id="authors">'+authors+'</p>');
-    //     $('.twelve').children().first().append('<p><a href="'+link+'">LINK</a></p>');
-    // })// #old click
+        $('.book-container').prepend("<div class='bookcard mdl-grid'>");
+        $('.book-container').children().first().append('<div  class="img mdl-cell mdl-cell--4-col"></div>');
+        $('.bookcard div:eq(0)').append('<img src='+image+'>');
+        $('.book-container').children().first().append('<div  class="book-info mdl-cell mdl-cell--6-col"></div>');
+        $('.bookcard div:eq(1)').append('<p id="title"><strong>Title:</strong> <u>'+ title +'</u></p>');
+        $('.bookcard div:eq(1)').children().first().append('<p id="authors"><strong>Authors:</strong> '+authors+'</p>');
+        $('.bookcard div:eq(1)').children().first().append('<p id="era"><strong>Literary Era:</strong> '+itemId+'</p>');
+        $('.bookcard div:eq(1)').children().first().append('<p><a href="'+link+'" target="_blank">Book Preview</a></p>');
+    })
+
+
+
 
 
 })();
+
+    // This example requires the Places library. Include the libraries=places
+    // parameter when you first load the API. For example:
+    // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+    var map;
+    var infowindow;
+
+    function initMap() {
+      var local = {lat: 40.740255, lng: -73.990674};
+
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: local,
+        zoom: 15
+      });
+
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: local,
+        radius: 2000,
+        type: ['library']
+      }, callback);
+    }
+
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    }
+
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+    }
